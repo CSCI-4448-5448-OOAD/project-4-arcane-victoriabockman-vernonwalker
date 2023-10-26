ec9e2364afc249d0bfcc420bb5ac951ad4bde6e7
victoriabockman <victoria.bockman@colorado.edu>
2023-10-26 16:23:25 -0600

    testing done

--- 3.1/src/ooad/test/tests.java
+++ 3.1/src/ooad/test/tests.java
@@ -3,9 +3,10 @@ package ooad.test;
 import org.junit.jupiter.api.Test;
 
 import ooad.arcane.Adventurer.Adventurer;
-import ooad.arcane.Adventurer.EmberKnight;
+import ooad.arcane.Adventurer.AdventurerFactory;
 import ooad.arcane.Creatures.Creatures;
 import ooad.arcane.Creatures.Terravore;
+import ooad.arcane.GameObservers.GameObserver;
 import ooad.arcane.RollBonus.MasterStrategy;
 import ooad.arcane.RollBonus.NoviceStrategy;
 import ooad.arcane.RollBonus.SeasonedStrategy;
@@ -21,11 +22,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-//  For full bonus points,
 // you must document how you run your JUnit tests (e.g., with a command line or in the IDE), and you
 // must capture output that shows the results of your tests. 
 
-// at least 10 tests
+// at least 15 tests
+
+// We will build off of our last tests to ensure they are still functioning with the changes we made
+// and add 5 more to test other functionalilty that was added
 
 
 
@@ -34,13 +37,17 @@ public class tests{
     @Test
     // testing for valid combat roll implmentation
     void battletest1(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
+        //Adventurer ads = new EmberKnight(null);
         Creatures cre = new Terravore(0);
         Room room = new Room();
 
-        ads.treasure.combatRollBonus = 4;
+        ad.treasure.combatRollBonus = 4;
 
-        Battle(ads, cre, room);
+        Battle(ad, cre, room);
 
         assert(cre.isAlive == false);
     }
@@ -48,62 +55,75 @@ public class tests{
     @Test
     // testing for valid strategy update
     void battletest2(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Creatures cre = new Terravore(0);
         Room room = new Room();
 
-        ads.treasure.combatRollBonus = 4;
+        ad.treasure.combatRollBonus = 4;
 
-        Battle(ads, cre, room);
+        Battle(ad, cre, room);
         
-        assert(ads.attackStrategy instanceof SeasonedStrategy);
+        assert(ad.attackStrategy instanceof SeasonedStrategy);
     }
 
     @Test
     // testing for limit to strategy update
     void battletest3(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Creatures cre = new Terravore(0);
         Room room = new Room();
 
-        ads.attackStrategy = new MasterStrategy();
+        ad.attackStrategy = new MasterStrategy();
 
-        Battle(ads, cre, room);
+        Battle(ad, cre, room);
         
-        assert(ads.attackStrategy instanceof MasterStrategy);
+        assert(ad.attackStrategy instanceof MasterStrategy);
     }
 
 
     @Test
     // testing for correct implementation of health bonus
     void battletest4(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Creatures cre = new Terravore(0);
         Room room = new Room();
 
-        ads.health = 2;
-        ads.treasure.healthBonus = 1;
+        ad.health = 2;
+        ad.treasure.healthBonus = 1;
 
-        Battle(ads, cre, room);
+        Battle(ad, cre, room);
         
-        assert(ads.health == 10);
+        assert(ad.health == 10);
     }
 
     @Test
     // testing for correct implementation of dodge chance
     void battletest5(){
         for(int j = 0; j < 10; j++){
-            Adventurer ads = new EmberKnight(null);
+            Rooms rooms = new Rooms();
+            GameObserver[] tracker = new GameObserver[2];
+
+            Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
             Creatures cre = new Terravore(0);
             Room room = new Room();
 
             // dodge chance initiated to 0.2F
-            ads.treasure.addedDodgeChance = 0.8F;
-            ads.health = 5;
+            ad.treasure.addedDodgeChance = 0.8F;
+            //ad.health = 5;
+            ad.health = 5;
 
-            Battle(ads, cre, room);
+            Battle(ad, cre, room);
             
-            assert(ads.health == 10);
+            assert(ad.health == 10);
         }
     }
             // 10 out of 10 times, the adventurer should dodge the attack
@@ -113,18 +133,21 @@ public class tests{
     
     void treasuretest1(){
     // testing for valid searchStrategy bonus implmentation
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Room cur_room = new Room();
         Rooms tel_room = new Rooms();
 
        tel_room.airFloor.rooms[1] = cur_room;
 
         cur_room.treasure = new SwordDecorator();
-        ads.searchStrategy = new MasterStrategy();
+        ad.searchStrategy = new MasterStrategy();
 
-        findTreasure(ads, cur_room, tel_room);
+        findTreasure(ad, cur_room, tel_room);
 
-        assert(ads.treasure.value == 1100);
+        assert(ad.treasure.value == 1100);
 
 
     }
@@ -132,44 +155,53 @@ public class tests{
     @Test
      // testing for valid strategy update (WHILE NOT UPDATING COMBAT STRAT AT THE SAME TIME)
     void treasuretest2(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Room cur_room = new Room();
         Rooms tel_room = new Rooms();
 
        tel_room.airFloor.rooms[1] = cur_room;
 
         cur_room.treasure = new SwordDecorator();
-        ads.searchStrategy = new VeteranStrategy();
+        ad.searchStrategy = new VeteranStrategy();
 
-        findTreasure(ads, cur_room, tel_room);
+        findTreasure(ad, cur_room, tel_room);
 
-        assert(ads.searchStrategy instanceof MasterStrategy);
-        assert(ads.attackStrategy instanceof NoviceStrategy);
+        assert(ad.searchStrategy instanceof MasterStrategy);
+        assert(ad.attackStrategy instanceof NoviceStrategy);
     }
 
     @Test
     // Portal test
     void treasuretest3(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Room cur_room = new Room();
         Rooms tel_room = new Rooms();
 
        tel_room.airFloor.rooms[1] = cur_room;
 
-       ads.currentRoom = cur_room;
+       ad.currentRoom = cur_room;
 
         cur_room.treasure = new PortalDecorator();
-        ads.searchStrategy = new MasterStrategy();
+        ad.searchStrategy = new MasterStrategy();
 
-        findTreasure(ads, cur_room, tel_room);
+        findTreasure(ad, cur_room, tel_room);
 
-        assert(ads.currentRoom != cur_room);
+        assert(ad.currentRoom != cur_room);
     }
 
     @Test
     // Testing to make sure treasure is added when applicable
     void treasuretest4(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Room cur_room = new Room();
         Rooms tel_room = new Rooms();
 
@@ -177,17 +209,20 @@ public class tests{
 
         cur_room.treasure = new SwordDecorator();
         
-        ads.searchStrategy = new MasterStrategy();
+        ad.searchStrategy = new MasterStrategy();
 
-        findTreasure(ads, cur_room, tel_room);
+        findTreasure(ad, cur_room, tel_room);
 
-        assert(ads.treasure.contents.contains("Sword"));
+        assert(ad.treasure.contents.contains("Sword"));
     }
 
     @Test
     // Testing to make sure treasure is NOT added when applicable
     void treasuretest5(){
-        Adventurer ads = new EmberKnight(null);
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
         Room cur_room = new Room();
         Rooms tel_room = new Rooms();
 
@@ -195,20 +230,147 @@ public class tests{
 
         cur_room.treasure = new SwordDecorator();
         
-        ads.searchStrategy = new MasterStrategy();
+        ad.searchStrategy = new MasterStrategy();
 
-        ads.treasure.contents.add("Sword");
+        ad.treasure.contents.add("Sword");
 
-        findTreasure(ads, cur_room, tel_room);
+        findTreasure(ad, cur_room, tel_room);
 
-        assert(!(ads.treasure.contents.indexOf("Sword") == 0 && ads.treasure.contents.indexOf("Sword") == 1));
+        assert(!(ad.treasure.contents.indexOf("Sword") == 0 && ad.treasure.contents.indexOf("Sword") == 1));
+    }
+
+    @Test
+    // Search Command uses new function execute, for ease of implementation (not messing with the logger / tracker)
+    // I pasted it below, so we are using that function instead
+
+    // Search Command, Winning
+    void commandtest1(){
+        
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
+
+        Room cur_room = new Room();
+
+        ad.currentRoom = cur_room;
+
+        ad.searchStrategy = new MasterStrategy();
+
+        cur_room.treasure = new SwordDecorator();
+
+        search_execute(ad);
+
+        assert(ad.treasure.contents.contains("Sword"));
+    }
+
+    @Test
+    // Search Command, Losing
+    void commandtest2(){
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
+
+        Room cur_room = new Room();
+
+        ad.currentRoom = cur_room;
+
+        cur_room.treasure = new SwordDecorator();
+
+        search_execute(ad);
+
+        assert(!ad.treasure.contents.contains("Sword"));
+    }
+
+    @Test
+    // Attack Command, Winning
+    void commandtest3(){
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
+
+        Creatures cre = new Terravore(0);
+
+        Room cur_room = new Room();
+
+        ad.health = 15;
+        ad.currentRoom = cur_room;
+        ad.treasure.combatRollBonus = 1;
+        ad.treasure.creatureCombatRollPenalty = 1;
+
+        cre.currentRoom = cur_room;
+        ad.currentRoom.evilOccupants.add(cre);
+
+        attack_execute(ad);
+
+        assert(ad.health == 15);
+    }
+
+    @Test
+    // Attack Command, Winning continued
+    void commandtest4(){
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
+
+        Creatures cre = new Terravore(0);
+
+        Room cur_room = new Room();
+
+        ad.health = 15;
+        ad.currentRoom = cur_room;
+        ad.treasure.combatRollBonus = 1;
+        ad.treasure.creatureCombatRollPenalty = 1;
+
+        cre.currentRoom = cur_room;
+        ad.currentRoom.evilOccupants.add(cre);
+
+        attack_execute(ad);
+
+        assert(cre.isAlive == false);
+    }
+
+    @Test
+    // Attack Command, Losing
+    void commandtest5(){
+        Rooms rooms = new Rooms();
+        GameObserver[] tracker = new GameObserver[2];
+
+        Adventurer ad = AdventurerFactory.createAdventurer("EmberKnight", rooms, tracker);
+
+        Creatures cre = new Terravore(0);
+
+        Room cur_room = new Room();
+
+        ad.health = 15;
+        ad.currentRoom = cur_room;
+
+        cre.currentRoom = cur_room;
+        ad.currentRoom.evilOccupants.add(cre);
+
+        attack_execute(ad);
+
+        // Changed health in losing battle to 10, to test for correct loss
+        assert(ad.health == 10);
     }
 
 
 
-
-
     // ADDED the findTreasure method here for ease and modification (to exclude random variables for testing)
+
+
+ public void search_execute(Adventurer adventurer){
+        findTreasure(adventurer, adventurer.currentRoom, adventurer.rooms_ad);
+    }
+
+public void attack_execute(Adventurer adventurer){
+        for(int i = 0; i < adventurer.currentRoom.evilOccupants.size(); i++){
+            Battle(adventurer, adventurer.currentRoom.evilOccupants.get(i), adventurer.currentRoom);
+        }
+    }
 
 
 public void findTreasure(Adventurer goodGuy, Room currentRoom, Rooms tele_room){
@@ -365,7 +527,7 @@ public void Battle(Adventurer goodGuy, Creatures badGuy, Room currentRoom){
                     }
                     else{
                         goodGuy.health -= 2;
-                        goodGuy.notify("adventurerLostHealth");
+             //           goodGuy.notify("adventurerLostHealth");
                     }
 
                   break;
@@ -381,15 +543,15 @@ public void Battle(Adventurer goodGuy, Creatures badGuy, Room currentRoom){
                     else{
                         if(currentRoom.coordinates.floor == "earth"){
                             goodGuy.health -= 1;
-                            goodGuy.notify("adventurerLostHealth");
+               //             goodGuy.notify("adventurerLostHealth");
                         }
                         else if (currentRoom.coordinates.floor == "fire"){
                             goodGuy.health -= 3;
-                            goodGuy.notify("adventurerLostHealth");
+               //             goodGuy.notify("adventurerLostHealth");
                         }
                         else{
                             goodGuy.health -= 2;
-                            goodGuy.notify("adventurerLostHealth");
+               //             goodGuy.notify("adventurerLostHealth");
                         }
                     }
 
@@ -405,7 +567,7 @@ public void Battle(Adventurer goodGuy, Creatures badGuy, Room currentRoom){
                     }
                     else{
                         goodGuy.health -= 2;
-                        goodGuy.notify("adventurerLostHealth");
+                 //       goodGuy.notify("adventurerLostHealth");
                     }
 
                   break;
@@ -425,7 +587,7 @@ public void Battle(Adventurer goodGuy, Creatures badGuy, Room currentRoom){
             // adventurer dead, battle over
             goodGuy.isAlive = false;
             currentRoom.alliedOccupants.remove(goodGuy);
-            badGuy.notify("creatureWonCombat",goodGuy);
+        //    badGuy.notify("creatureWonCombat",goodGuy);
 
            }
         }
